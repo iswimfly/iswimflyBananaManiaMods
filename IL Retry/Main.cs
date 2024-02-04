c5a05fad1cacaf1dd687f1157c3de29ac67c1cf5
iswimfly <iswimfly556@gmail.com>
2024-02-04 17:43:38 -0500

    IL Retry
    Wahoo

---
+++ IL Retry/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Flash2;
+
+namespace DeadLineReq
+{
+    public class Main
+    {
+        // AppInput controller to get controller inputs for P1
+        private static AppInput.state_t mgState_t = null;
+
+        public static void OnModUpdate()
+        {
+            // If the AppInput slot 0 is active, grab it and store it for later.
+            if (AppInput.ActiveState(0) != null && mgState_t == null)
+            {
+                mgState_t = AppInput.ActiveState(0);
+                Console.WriteLine("AppInput Found! P1 Grabbed.");
+            }
+
+            if (MainGame.Exists && MainGame.gameKind != MainGameDef.eGameKind.Practice)
+            {
+                // Default keybinds for quick retry.
+                if (mgState_t.m_buttonMaskDown == Def.Button.Y || Input.GetKeyDown(KeyCode.X))
+                {
+                    // If the game is in an unpausable state, do not retry
+                    if (MainGame.mainGameStage.check_pausable() == false) return;
+                    MainGame.NotifyEvent(MainGame.eEvent.Retry);
+                }
+            }
+        }
+
+    }
+}
---
+++ IL Retry/readme.md
@@ -0,0 +1,5 @@
+# IL Retry Mod
+
+This mod provides speedrunners with a quick tool to retry stages in modes you normally would not be able to. Every mode including practice mode now has the same retry button as practice mode. Requested by Dead Line!
+
+Since this hooks into controller inputs and I couldn't be bothered to figure out rebinding, it's stuck to Y/△ on controller, or X on keyboard.
